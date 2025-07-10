8f329873eb4f706b8e28967998e1e6f79f22d00f
pswbuild <pswbuild@oppo.com>
2025-07-10 18:22:50 +0800

    Synchronize code for OnePlus CPH2621_15.0.0.1010(EX01) Based on QCOM release TAG:AU_LINUX_KERNEL.PLATFORM.3.0.R6.00.00.00.043.011

--- drivers/clk/qcom/gcc-sm8250.c
+++ drivers/clk/qcom/gcc-sm8250.c
@@ -3905,7 +3905,7 @@ static struct gdsc pcie_0_gdsc = {
 	.pd = {
 		.name = "pcie_0_gdsc",
 	},
-	.pwrsts = PWRSTS_OFF_ON,
+	.pwrsts = PWRSTS_RET_ON,
 };
 
 static struct gdsc pcie_1_gdsc = {
@@ -3913,7 +3913,7 @@ static struct gdsc pcie_1_gdsc = {
 	.pd = {
 		.name = "pcie_1_gdsc",
 	},
-	.pwrsts = PWRSTS_OFF_ON,
+	.pwrsts = PWRSTS_RET_ON,
 };
 
 static struct gdsc pcie_2_gdsc = {
@@ -3921,7 +3921,7 @@ static struct gdsc pcie_2_gdsc = {
 	.pd = {
 		.name = "pcie_2_gdsc",
 	},
-	.pwrsts = PWRSTS_OFF_ON,
+	.pwrsts = PWRSTS_RET_ON,
 };
 
 static struct gdsc ufs_card_gdsc = {
